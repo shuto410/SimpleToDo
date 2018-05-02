d8ede2ab436cf522915af00b0cb87148cbba0bd5
shuto <shuto@posl.ait.kyushu-u.ac.jp>
2018-05-03 00:12:37 +0900

    ユーザ名表示

--- js/service.js
+++ js/service.js
@@ -1,8 +1,8 @@
 window.addEventListener('load', function(){
-    getSession(getUserName(id, function(name){
+    getSession(getUserName, function(name){
         $('#userid').text(name+"さんがログインしています");
     })
-);
+});
     
     /*getSession().then(function(id){
         $('#userid').text(id);
@@ -10,7 +10,44 @@ window.addEventListener('load', function(){
         console.log(error);
         window.location.href = 'index.html';
     })*/
-})
+
+//ログインidよりユーザ名の取得
+var getUserName = function(id, resolve){
+    fetch("php/getUserName.php", {
+        method: 'POST',
+        body:   'id=' + id,
+        headers: new Headers({
+            'Content-type': 'application/x-www-form-urlencoded'
+        })
+    }).then(function(response){
+        return response.json();
+    }).then(function(json){
+        if(json.isSuccess == true){
+            resolve(json.name);
+        } 
+    })
+}
+
+
+var getSession = function(resolve, showName){
+    $.ajax({
+        url: "php/getSession.php",
+        type: "POST",
+        dataType: "json",
+    })
+    .done(function(json){
+        //alert(json.sessionId);
+        if(json.isSuccess == true){
+            //alert("get success");
+            console.log("get session success");
+            resolve(json.id, showName);
+        }
+        else{
+            //alert("get failed");
+            console.log("get session failed"+json.id);
+        }
+    });
+}
 
 /*$(function(){
     $('#get_id').click(function(){
@@ -50,23 +87,3 @@ $(function(){
         })
     })
 })
-
-//ログインidよりユーザ名の取得
-var getUserName = function(id, resolve, reject){
-    fetch("php/getUserName.php", {
-        method: 'POST',
-        body:   'id=' + id,
-        headers: new Headers({
-            'Content-type': 'application/x-www-form-urlencoded'
-        })
-    }).then(function(response){
-        return response.json();
-    }).then(function(json){
-        if(json.isSuccess == true){
-            resolve(json.name);
-        } 
-        else{
-            reject();
-        }
-    })
-}
