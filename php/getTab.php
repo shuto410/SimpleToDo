eb48c7742f114c72c61d0c2048ba8480fa55a4cf
shuto <shuto@posl.ait.kyushu-u.ac.jp>
2018-05-21 18:06:00 +0900

    getTab()においてTabのIDを返すようにした

--- php/getTab.php
+++ php/getTab.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 
 //引数のユーザIDのタブ名を配列で取得する
 function getTab(){
-    $result = array('isSuccess' => false, 'size' => 0, 'tab_list' => array());
+    $result = array('isSuccess' => false, 'size' => 0, 'tab_list' => array(), 'id_list' => array());
 
     if(isset($_POST['user_id'])){
         $user_id = sanitizeString($_POST['user_id']);
@@ -17,6 +17,7 @@ function getTab(){
             for($i = 0; $i < $result['size']; $i++){
                 $row = mysqli_fetch_array($queryResponse, MYSQLI_ASSOC);
                 array_push($result['tab_list'], $row['name']);
+                array_push($result['id_list'], $row['id']);
             }
         }
     }
